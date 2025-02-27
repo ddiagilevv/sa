e4ab8020c8bf8aa00db7921aab2947a5c20c0824
ddiagilevv <134507138+ddiagilevv@users.noreply.github.com>
2025-02-27 19:15:05 +0400

    Add hometask1

--- lesson-1/1-vars.js
+++
@@ -1,5 +0,0 @@
-let name = "Анна";
-let age = 25;
-
-console.log("Меня зовут", name);
-console.log("Мне", age, "лет");
---
+++ lesson-1/program.js
@@ -0,0 +1,15 @@
+let name = "Анна";
+let age = 25;
+
+console.log("Меня зовут", name);
+console.log("Мне", age, "лет");
+
+/* 
+    кроссплатформенный (front: react, angular... back: nodejs; react native ...)
+    динамически типизированный
+    слабо типизированный
+    интерпретируемы. В NodeJS - Just in Time компиляция
+    поддержка асинхронности
+    ООП
+    высокоуровневый
+*/
