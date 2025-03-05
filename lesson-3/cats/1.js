b06e61e6623656e27b5673a6234f401ee96e92a9
ddiagilevv <134507138+ddiagilevv@users.noreply.github.com>
2025-03-05 14:28:01 +0400

    additions

--- lesson-3/cats/1.js
+++ lesson-3/cats/1.js
@@ -1,11 +1,1 @@
-let cat_1 = {
-    name: "Yaqub Qamar Ad-Din Dabiazah",
-    age: 2,
-    color: "рыжий",
-    speak: function() {
-        console.log(this.name + "Мяу!");
-    },
-    action: function() {
-        console.log(this.name + "крадётся....");
-    }
-};
+//let cat_1 = 
