f0b8a8d848df33dcfe06b1c6c882e84e8ebb6785
java-strip <rita.kalash@icloud.com>
2025-04-02 20:05:06 +0700

    Update binary-tree.js

--- javascript/3-data-structures/non-linear/trees/binary-tree/binary-tree.js
+++ javascript/3-data-structures/non-linear/trees/binary-tree/binary-tree.js
@@ -47,5 +47,47 @@ class BinarySearchTree {
 
         }
 
+    }
+    remove(value) {
+        //надо найти узел, где хранится value, удалить его, корректно сохранив структуру дерева
+        // 1.узел без детей
+        // 2.узел с 1 потомком - меняем удаленный на потомка 
+        // 3.узел с 2 потомками - найти минимум в правом поддереве,
+        //затем заменить value на этот минимум, затем удалить дубликат минимум
+        //в правом поддереве.
+
+        // функция ищет самый левый узел в поддереве, начиная с node 
+        function findMin(node) {
+            while (node.left !== null){
+                node = node.left;
+            }
+            return node;
+        }
+
+        function removeNode(node, value) {
+            if (node === null) return null;
+
+            if (value < node.value){
+                node.left = removeNode(node.left, value);
+                return node;
+            } else if (value > node.value){
+                node.right = removeNode(node.right, value);
+                return node;
+            } else {
+                if (node.left === null && node.right === null) return null;
+                if (node.left === null) return node.right;
+                if (node.right === null) return node.left;
+
+                //ищем самый левый узел в ПРАВОМ поддереве
+                const minNode = findMin(node.right);
+                node.value = minNode.value;
+                node.right = removeNode(node.right, minNode.value);
+                return node;
+            }
+
+        }
+
+        this.root = removeNode(this.root, value);
+
     } 
 }
