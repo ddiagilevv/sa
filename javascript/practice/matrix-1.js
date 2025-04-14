bef87b72782177883b41b7d87bb8ae9b80cea163
java-strip <rita.kalash@icloud.com>
2025-04-14 19:52:20 +0700

    Update matrix-1.js

--- javascript/practice/matrix-1.js
+++ javascript/practice/matrix-1.js
@@ -6,3 +6,19 @@ const matrix = [
     [0, 8, 0]
   ];
   
+let n = matrix.length; //количество строк 
+let m = matrix[0].length; //количество столбцов
+let i = 0;
+let zeroCount = 0;
+
+while (i < n) {
+  let j = 0;
+  while (j < m) {
+    if (matrix[i][j] === 0) {
+      zeroCount++;
+    }
+    j++;
+  }
+  i++;
+}
+console.log(`Количество нулей: ${zeroCount}`);
